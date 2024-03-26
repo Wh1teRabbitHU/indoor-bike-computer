92cf146556a606105630bf25dfd808719756219b
Tamas Ruszka <ruszka.tamas@gmail.com>
2024-03-26 20:51:33 +0100

    Small changes

--- Firmware/STM32F446/Core/Src/Components/GUI/Elements/gui_tab_main.c
+++ Firmware/STM32F446/Core/Src/Components/GUI/Elements/gui_tab_main.c
@@ -7,6 +7,7 @@ GUI_TabMain GUI_TabMain_create(GUI_TabMain_Config* config) {
 
     lv_obj_t* tabView = lv_tabview_create(config->screen);
 
+    lv_obj_set_style_bg_color(tabView, lv_color_hex(GUI_TABVIEW_TABMAIN_BGCOLOR), 0);
     lv_tabview_set_tab_bar_position(tabView, LV_DIR_BOTTOM);
     lv_tabview_set_tab_bar_size(tabView, config->height);
 
@@ -14,12 +15,16 @@ GUI_TabMain GUI_TabMain_create(GUI_TabMain_Config* config) {
     lv_obj_t* tabHistory = lv_tabview_add_tab(tabView, "Hist.");
     lv_obj_t* tabSettings = lv_tabview_add_tab(tabView, "Set.");
 
-    lv_obj_set_style_bg_color(tabView, lv_color_hex(GUI_TABVIEW_TABMAIN_BGCOLOR), 0);
-    lv_obj_set_style_bg_color(tabMain, lv_color_hex(GUI_TABVIEW_TABMAIN_BGCOLOR), 0);
+    lv_obj_set_style_pad_all(tabMain, 0, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(tabMain, lv_color_hex(GUI_TABVIEW_TABMAIN_BGCOLOR), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(tabMain, LV_OPA_COVER, 0);
-    lv_obj_set_style_bg_color(tabHistory, lv_color_hex(GUI_TABVIEW_TABHISTORY_BGCOLOR), 0);
+
+    lv_obj_set_style_pad_all(tabHistory, 0, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(tabHistory, lv_color_hex(GUI_TABVIEW_TABHISTORY_BGCOLOR), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(tabHistory, LV_OPA_COVER, 0);
-    lv_obj_set_style_bg_color(tabSettings, lv_color_hex(GUI_TABVIEW_TABSETTINGS_BGCOLOR), 0);
+
+    lv_obj_set_style_pad_all(tabSettings, 0, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(tabSettings, lv_color_hex(GUI_TABVIEW_TABSETTINGS_BGCOLOR), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(tabSettings, LV_OPA_COVER, 0);
 
     tabInstance.tabView = tabView;
