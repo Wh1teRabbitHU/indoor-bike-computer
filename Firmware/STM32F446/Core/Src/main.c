9ca14b781eb464a7a2d65532064353ddcc7cb9b7
Tamas Ruszka <ruszka.tamas@gmail.com>
2024-12-17 20:35:16 +0100

    Reformating files

--- Firmware/STM32F446/Core/Src/main.c
+++ Firmware/STM32F446/Core/Src/main.c
@@ -18,7 +18,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
 #include "fatfs.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -111,15 +110,15 @@ int main(void) {
     MX_SDIO_SD_Init();
     MX_USART3_UART_Init();
     MX_RTC_Init();
-    MX_FATFS_Init();
     MX_TIM14_Init();
+    MX_FATFS_Init();
     /* USER CODE BEGIN 2 */
 
     GUI_init();
     MCP3421_init(&hi2c2);
     Data_initStorage();
     Data_loadStatistics();
-    HAL_TIM_Base_Start_IT(&htim14);  // Start timer
+    HAL_TIM_Base_Start_IT(&htim14); // Start timer
 
     // SDCard_writeLine("/test_long_filename.txt", "Testing the SD card library...\n");
     // SDCard_DirPage dirPage = {0};
@@ -166,16 +165,16 @@ void SystemClock_Config(void) {
      * in the RCC_OscInitTypeDef structure.
      */
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-    RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM = 4;
-    RCC_OscInitStruct.PLL.PLLN = 180;
-    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ = 8;
-    RCC_OscInitStruct.PLL.PLLR = 2;
+    RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
+    RCC_OscInitStruct.LSEState       = RCC_LSE_ON;
+    RCC_OscInitStruct.LSIState       = RCC_LSI_ON;
+    RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM       = 4;
+    RCC_OscInitStruct.PLL.PLLN       = 180;
+    RCC_OscInitStruct.PLL.PLLP       = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ       = 8;
+    RCC_OscInitStruct.PLL.PLLR       = 2;
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
         Error_Handler();
     }
@@ -188,9 +187,9 @@ void SystemClock_Config(void) {
 
     /** Initializes the CPU, AHB and APB buses clocks
      */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
@@ -205,6 +204,7 @@ void SystemClock_Config(void) {
  * @retval None
  */
 static void MX_I2C1_Init(void) {
+
     /* USER CODE BEGIN I2C1_Init 0 */
 
     /* USER CODE END I2C1_Init 0 */
@@ -212,15 +212,15 @@ static void MX_I2C1_Init(void) {
     /* USER CODE BEGIN I2C1_Init 1 */
 
     /* USER CODE END I2C1_Init 1 */
-    hi2c1.Instance = I2C1;
-    hi2c1.Init.ClockSpeed = 100000;
-    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-    hi2c1.Init.OwnAddress1 = 0;
-    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+    hi2c1.Instance             = I2C1;
+    hi2c1.Init.ClockSpeed      = 100000;
+    hi2c1.Init.DutyCycle       = I2C_DUTYCYCLE_2;
+    hi2c1.Init.OwnAddress1     = 0;
+    hi2c1.Init.AddressingMode  = I2C_ADDRESSINGMODE_7BIT;
     hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-    hi2c1.Init.OwnAddress2 = 0;
+    hi2c1.Init.OwnAddress2     = 0;
     hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+    hi2c1.Init.NoStretchMode   = I2C_NOSTRETCH_DISABLE;
     if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
         Error_Handler();
     }
@@ -235,6 +235,7 @@ static void MX_I2C1_Init(void) {
  * @retval None
  */
 static void MX_I2C2_Init(void) {
+
     /* USER CODE BEGIN I2C2_Init 0 */
 
     /* USER CODE END I2C2_Init 0 */
@@ -242,15 +243,15 @@ static void MX_I2C2_Init(void) {
     /* USER CODE BEGIN I2C2_Init 1 */
 
     /* USER CODE END I2C2_Init 1 */
-    hi2c2.Instance = I2C2;
-    hi2c2.Init.ClockSpeed = 100000;
-    hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
-    hi2c2.Init.OwnAddress1 = 0;
-    hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+    hi2c2.Instance             = I2C2;
+    hi2c2.Init.ClockSpeed      = 100000;
+    hi2c2.Init.DutyCycle       = I2C_DUTYCYCLE_2;
+    hi2c2.Init.OwnAddress1     = 0;
+    hi2c2.Init.AddressingMode  = I2C_ADDRESSINGMODE_7BIT;
     hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-    hi2c2.Init.OwnAddress2 = 0;
+    hi2c2.Init.OwnAddress2     = 0;
     hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-    hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+    hi2c2.Init.NoStretchMode   = I2C_NOSTRETCH_DISABLE;
     if (HAL_I2C_Init(&hi2c2) != HAL_OK) {
         Error_Handler();
     }
@@ -265,6 +266,7 @@ static void MX_I2C2_Init(void) {
  * @retval None
  */
 static void MX_RTC_Init(void) {
+
     /* USER CODE BEGIN RTC_Init 0 */
 
     /* USER CODE END RTC_Init 0 */
@@ -278,13 +280,13 @@ static void MX_RTC_Init(void) {
 
     /** Initialize RTC Only
      */
-    hrtc.Instance = RTC;
-    hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
-    hrtc.Init.AsynchPrediv = 127;
-    hrtc.Init.SynchPrediv = 255;
-    hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+    hrtc.Instance            = RTC;
+    hrtc.Init.HourFormat     = RTC_HOURFORMAT_24;
+    hrtc.Init.AsynchPrediv   = 127;
+    hrtc.Init.SynchPrediv    = 255;
+    hrtc.Init.OutPut         = RTC_OUTPUT_DISABLE;
     hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
-    hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+    hrtc.Init.OutPutType     = RTC_OUTPUT_TYPE_OPENDRAIN;
     if (HAL_RTC_Init(&hrtc) != HAL_OK) {
         Error_Handler();
     }
@@ -295,18 +297,18 @@ static void MX_RTC_Init(void) {
 
     /** Initialize RTC and set the Time and Date
      */
-    sTime.Hours = 0;
-    sTime.Minutes = 0;
-    sTime.Seconds = 0;
+    sTime.Hours          = 0;
+    sTime.Minutes        = 0;
+    sTime.Seconds        = 0;
     sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
     sTime.StoreOperation = RTC_STOREOPERATION_RESET;
     if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN) != HAL_OK) {
         Error_Handler();
     }
     sDate.WeekDay = RTC_WEEKDAY_MONDAY;
-    sDate.Month = RTC_MONTH_JANUARY;
-    sDate.Date = 1;
-    sDate.Year = 0;
+    sDate.Month   = RTC_MONTH_JANUARY;
+    sDate.Date    = 1;
+    sDate.Year    = 0;
 
     if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BIN) != HAL_OK) {
         Error_Handler();
@@ -328,6 +330,7 @@ static void MX_RTC_Init(void) {
  * @retval None
  */
 static void MX_SDIO_SD_Init(void) {
+
     /* USER CODE BEGIN SDIO_Init 0 */
 
     /* USER CODE END SDIO_Init 0 */
@@ -335,13 +338,13 @@ static void MX_SDIO_SD_Init(void) {
     /* USER CODE BEGIN SDIO_Init 1 */
 
     /* USER CODE END SDIO_Init 1 */
-    hsd.Instance = SDIO;
-    hsd.Init.ClockEdge = SDIO_CLOCK_EDGE_RISING;
-    hsd.Init.ClockBypass = SDIO_CLOCK_BYPASS_DISABLE;
-    hsd.Init.ClockPowerSave = SDIO_CLOCK_POWER_SAVE_DISABLE;
-    hsd.Init.BusWide = SDIO_BUS_WIDE_1B;
+    hsd.Instance                 = SDIO;
+    hsd.Init.ClockEdge           = SDIO_CLOCK_EDGE_RISING;
+    hsd.Init.ClockBypass         = SDIO_CLOCK_BYPASS_DISABLE;
+    hsd.Init.ClockPowerSave      = SDIO_CLOCK_POWER_SAVE_DISABLE;
+    hsd.Init.BusWide             = SDIO_BUS_WIDE_1B;
     hsd.Init.HardwareFlowControl = SDIO_HARDWARE_FLOW_CONTROL_DISABLE;
-    hsd.Init.ClockDiv = 24;
+    hsd.Init.ClockDiv            = 24;
     /* USER CODE BEGIN SDIO_Init 2 */
 
     /* USER CODE END SDIO_Init 2 */
@@ -353,6 +356,7 @@ static void MX_SDIO_SD_Init(void) {
  * @retval None
  */
 static void MX_TIM14_Init(void) {
+
     /* USER CODE BEGIN TIM14_Init 0 */
 
     /* USER CODE END TIM14_Init 0 */
@@ -360,11 +364,11 @@ static void MX_TIM14_Init(void) {
     /* USER CODE BEGIN TIM14_Init 1 */
 
     /* USER CODE END TIM14_Init 1 */
-    htim14.Instance = TIM14;
-    htim14.Init.Prescaler = 9000 - 1;
-    htim14.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim14.Init.Period = 2000 - 1;
-    htim14.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    htim14.Instance               = TIM14;
+    htim14.Init.Prescaler         = 9000 - 1;
+    htim14.Init.CounterMode       = TIM_COUNTERMODE_UP;
+    htim14.Init.Period            = 2000 - 1;
+    htim14.Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1;
     htim14.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
     if (HAL_TIM_Base_Init(&htim14) != HAL_OK) {
         Error_Handler();
@@ -380,6 +384,7 @@ static void MX_TIM14_Init(void) {
  * @retval None
  */
 static void MX_USART3_UART_Init(void) {
+
     /* USER CODE BEGIN USART3_Init 0 */
 
     /* USER CODE END USART3_Init 0 */
@@ -387,13 +392,13 @@ static void MX_USART3_UART_Init(void) {
     /* USER CODE BEGIN USART3_Init 1 */
 
     /* USER CODE END USART3_Init 1 */
-    huart3.Instance = USART3;
-    huart3.Init.BaudRate = 115200;
-    huart3.Init.WordLength = UART_WORDLENGTH_8B;
-    huart3.Init.StopBits = UART_STOPBITS_1;
-    huart3.Init.Parity = UART_PARITY_NONE;
-    huart3.Init.Mode = UART_MODE_TX_RX;
-    huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart3.Instance          = USART3;
+    huart3.Init.BaudRate     = 115200;
+    huart3.Init.WordLength   = UART_WORDLENGTH_8B;
+    huart3.Init.StopBits     = UART_STOPBITS_1;
+    huart3.Init.Parity       = UART_PARITY_NONE;
+    huart3.Init.Mode         = UART_MODE_TX_RX;
+    huart3.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
     huart3.Init.OverSampling = UART_OVERSAMPLING_16;
     if (HAL_UART_Init(&huart3) != HAL_OK) {
         Error_Handler();
@@ -421,37 +426,28 @@ static void MX_GPIO_Init(void) {
     __HAL_RCC_GPIOD_CLK_ENABLE();
 
     /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(
-        GPIOC,
-        DISPLAY_D13_Pin | DISPLAY_D14_Pin | DISPLAY_D15_Pin | DISPLAY_D16_Pin | DISPLAY_D17_Pin | DISPLAY_RESET_Pin,
-        GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOC, DISPLAY_D13_Pin | DISPLAY_D14_Pin | DISPLAY_D15_Pin | DISPLAY_D16_Pin | DISPLAY_D17_Pin | DISPLAY_RESET_Pin, GPIO_PIN_RESET);
 
     /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(GPIOA,
-                      DISPLAY_D00_Pin | DISPLAY_D01_Pin | DISPLAY_D02_Pin | DISPLAY_D03_Pin | DISPLAY_D04_Pin |
-                          DISPLAY_D05_Pin | DISPLAY_D06_Pin | DISPLAY_D07_Pin | DISPLAY_D08_Pin | DISPLAY_D09_Pin |
-                          DISPLAY_D10_Pin | DISPLAY_D11_Pin | DISPLAY_D12_Pin,
-                      GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA, DISPLAY_D00_Pin | DISPLAY_D01_Pin | DISPLAY_D02_Pin | DISPLAY_D03_Pin | DISPLAY_D04_Pin | DISPLAY_D05_Pin | DISPLAY_D06_Pin | DISPLAY_D07_Pin | DISPLAY_D08_Pin | DISPLAY_D09_Pin | DISPLAY_D10_Pin | DISPLAY_D11_Pin | DISPLAY_D12_Pin, GPIO_PIN_RESET);
 
     /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(GPIOB, DISPLAY_RD_Pin | DISPLAY_WR_Pin | DISPLAY_DC_Pin | DISPLAY_CS_Pin | GAUGE_ALERT_Pin,
-                      GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOB, DISPLAY_RD_Pin | DISPLAY_WR_Pin | DISPLAY_DC_Pin | DISPLAY_CS_Pin | GAUGE_ALERT_Pin, GPIO_PIN_RESET);
 
     /*Configure GPIO pin Output Level */
     HAL_GPIO_WritePin(ESP32_EN_GPIO_Port, ESP32_EN_Pin, GPIO_PIN_SET);
 
     /*Configure GPIO pin : USER_BTN_4_Pin */
-    GPIO_InitStruct.Pin = USER_BTN_4_Pin;
+    GPIO_InitStruct.Pin  = USER_BTN_4_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(USER_BTN_4_GPIO_Port, &GPIO_InitStruct);
 
     /*Configure GPIO pins : DISPLAY_D13_Pin DISPLAY_D14_Pin DISPLAY_D15_Pin DISPLAY_D16_Pin
                              DISPLAY_D17_Pin DISPLAY_RESET_Pin */
-    GPIO_InitStruct.Pin =
-        DISPLAY_D13_Pin | DISPLAY_D14_Pin | DISPLAY_D15_Pin | DISPLAY_D16_Pin | DISPLAY_D17_Pin | DISPLAY_RESET_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pin   = DISPLAY_D13_Pin | DISPLAY_D14_Pin | DISPLAY_D15_Pin | DISPLAY_D16_Pin | DISPLAY_D17_Pin | DISPLAY_RESET_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
@@ -459,43 +455,41 @@ static void MX_GPIO_Init(void) {
                              DISPLAY_D04_Pin DISPLAY_D05_Pin DISPLAY_D06_Pin DISPLAY_D07_Pin
                              DISPLAY_D08_Pin DISPLAY_D09_Pin DISPLAY_D10_Pin DISPLAY_D11_Pin
                              DISPLAY_D12_Pin */
-    GPIO_InitStruct.Pin = DISPLAY_D00_Pin | DISPLAY_D01_Pin | DISPLAY_D02_Pin | DISPLAY_D03_Pin | DISPLAY_D04_Pin |
-                          DISPLAY_D05_Pin | DISPLAY_D06_Pin | DISPLAY_D07_Pin | DISPLAY_D08_Pin | DISPLAY_D09_Pin |
-                          DISPLAY_D10_Pin | DISPLAY_D11_Pin | DISPLAY_D12_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pin   = DISPLAY_D00_Pin | DISPLAY_D01_Pin | DISPLAY_D02_Pin | DISPLAY_D03_Pin | DISPLAY_D04_Pin | DISPLAY_D05_Pin | DISPLAY_D06_Pin | DISPLAY_D07_Pin | DISPLAY_D08_Pin | DISPLAY_D09_Pin | DISPLAY_D10_Pin | DISPLAY_D11_Pin | DISPLAY_D12_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /*Configure GPIO pins : SDIO_CD_Pin POWER_ALERT_Pin */
-    GPIO_InitStruct.Pin = SDIO_CD_Pin | POWER_ALERT_Pin;
+    GPIO_InitStruct.Pin  = SDIO_CD_Pin | POWER_ALERT_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
     /*Configure GPIO pins : DISPLAY_RD_Pin DISPLAY_WR_Pin DISPLAY_DC_Pin DISPLAY_CS_Pin
                              GAUGE_ALERT_Pin */
-    GPIO_InitStruct.Pin = DISPLAY_RD_Pin | DISPLAY_WR_Pin | DISPLAY_DC_Pin | DISPLAY_CS_Pin | GAUGE_ALERT_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pin   = DISPLAY_RD_Pin | DISPLAY_WR_Pin | DISPLAY_DC_Pin | DISPLAY_CS_Pin | GAUGE_ALERT_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
     /*Configure GPIO pin : ESP32_EN_Pin */
-    GPIO_InitStruct.Pin = ESP32_EN_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pin   = ESP32_EN_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_OD;
+    GPIO_InitStruct.Pull  = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(ESP32_EN_GPIO_Port, &GPIO_InitStruct);
 
     /*Configure GPIO pin : REVOLUTION_SIGNAL_Pin */
-    GPIO_InitStruct.Pin = REVOLUTION_SIGNAL_Pin;
+    GPIO_InitStruct.Pin  = REVOLUTION_SIGNAL_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(REVOLUTION_SIGNAL_GPIO_Port, &GPIO_InitStruct);
 
     /*Configure GPIO pins : USER_BTN_1_Pin USER_BTN_2_Pin USER_BTN_3_Pin */
-    GPIO_InitStruct.Pin = USER_BTN_1_Pin | USER_BTN_2_Pin | USER_BTN_3_Pin;
+    GPIO_InitStruct.Pin  = USER_BTN_1_Pin | USER_BTN_2_Pin | USER_BTN_3_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
@@ -536,7 +530,7 @@ void Error_Handler(void) {
  * @param  line: assert_param error line source number
  * @retval None
  */
-void assert_failed(uint8_t *file, uint32_t line) {
+void assert_failed(uint8_t * file, uint32_t line) {
     /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
